a91bc2d5c9fea632eda436b160c53df2aa538b21
sravya <35079205+sravya06@users.noreply.github.com>
2019-06-12 10:02:30 -0400

    Add files via upload

---
+++ TCP Socket Data Transfer/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace TCP_Socket_Data_Transfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_start_server = new System.Windows.Forms.Button();
+            this.tx_recv_data = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.tx_data_send = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_start_server
+            // 
+            this.btn_start_server.Location = new System.Drawing.Point(299, 76);
+            this.btn_start_server.Name = "btn_start_server";
+            this.btn_start_server.Size = new System.Drawing.Size(157, 23);
+            this.btn_start_server.TabIndex = 0;
+            this.btn_start_server.Text = "Start Server";
+            this.btn_start_server.UseVisualStyleBackColor = true;
+            this.btn_start_server.Click += new System.EventHandler(this.Btn_start_server_Click);
+            // 
+            // tx_recv_data
+            // 
+            this.tx_recv_data.Location = new System.Drawing.Point(178, 142);
+            this.tx_recv_data.Name = "tx_recv_data";
+            this.tx_recv_data.Size = new System.Drawing.Size(486, 20);
+            this.tx_recv_data.TabIndex = 1;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(604, 271);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 2;
+            this.btn_send.Text = "Send Data";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.Btn_send_Click);
+            // 
+            // tx_data_send
+            // 
+            this.tx_data_send.Location = new System.Drawing.Point(178, 190);
+            this.tx_data_send.Name = "tx_data_send";
+            this.tx_data_send.Size = new System.Drawing.Size(486, 20);
+            this.tx_data_send.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tx_data_send);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.tx_recv_data);
+            this.Controls.Add(this.btn_start_server);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_start_server;
+        private System.Windows.Forms.TextBox tx_recv_data;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.TextBox tx_data_send;
+    }
+}
+
---
+++ TCP Socket Data Transfer/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_Socket_Data_Transfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_start_server_Click(object sender, EventArgs e)
+        {
+            TCPServer obj_server = new TCPServer();
+            System.Threading.Thread obj_thread = new System.Threading.Thread(obj_server.StartServer);
+            obj_thread.Start();
+        }
+
+        private void Btn_send_Click(object sender, EventArgs e)
+        {
+            TcpClient tc = new TcpClient("127.0.0.1", 6868);
+            NetworkStream ns = tc.GetStream();
+            byte[] data_tosend = CreateDataPacket(Encoding.UTF8.GetBytes(tx_data_send.Text));
+            ns.Write(data_tosend, 0, data_tosend.Length);
+        }
+        private byte[] CreateDataPacket(byte[] data)
+        {
+            byte[] initialize = new byte[1];
+            initialize[0] = 2;
+            byte[] separator = new byte[1];
+            separator[0] = 4;
+            byte[] datalength = Encoding.UTF8.GetBytes(Convert.ToString(data.Length));
+            MemoryStream ms = new MemoryStream();
+            ms.Write(initialize, 0, initialize.Length);
+            ms.Write(datalength, 0, datalength.Length);
+            ms.Write(separator, 0, separator.Length);
+            ms.Write(data, 0, data.Length);
+            return ms.ToArray();
+        }
+    }
+    class TCPServer
+    {
+        TcpListener obj_server;
+        public TCPServer()
+        {
+            obj_server = new TcpListener(IPAddress.Any, 6868);
+        }
+
+        public void StartServer()
+        {
+            obj_server.Start();
+            while (true)
+            {
+                TcpClient tc = obj_server.AcceptTcpClient();
+                NetworkStream ns = tc.GetStream();
+                if (ns.ReadByte() == 2)
+                {
+                    byte[] recv_data = ReadStream(ns);
+                    Form1.ActiveForm.Invoke(new MethodInvoker(delegate
+                    {
+                        ((TextBox)Form1.ActiveForm.Controls.Find("tx_recv_data", true)[0]).Text = Encoding.UTF8.GetString(recv_data);
+                    }));
+                }
+            }
+        }
+
+        public byte[] ReadStream(NetworkStream ns)
+        {
+            byte[] data_buff = null;
+
+            int b = 0;
+            String buff_length = "";
+            while ((b = ns.ReadByte()) != 4)
+            {
+                buff_length += (char)b;
+            }
+            int data_length = Convert.ToInt32(buff_length);
+            data_buff = new byte[data_length];
+            int byte_read = 0;
+            int byte_offset = 0;
+            while (byte_offset < data_length)
+            {
+                byte_read = ns.Read(data_buff, byte_offset, data_length - byte_offset);
+                byte_offset += byte_read;
+            }
+
+            return data_buff;
+        }
+    }
+    }
